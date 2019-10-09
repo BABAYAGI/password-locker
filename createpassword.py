93bf1fe41573925aa2070c873d7ada8bc6682cc6
babayagi <darsan_hirani@hotmail.com>
2019-10-09 16:10:14 +0300

    added new approach

--- createpassword.py
+++ createpassword.py
@@ -1,23 +1,23 @@
-import pickle
-import random
+# import pickle
+# import random
 
-info = {}
+# info = {}
 
-with open("game.p", "br") as readfile:
-  info = pickle.load(readfile)
-print(info)
-s = "QWERTYUIOPLKJHGFDSAZXCVBNM,.;'[]=-qwertyuioplkjhgfdsazxcvbnm,.;']*-1234567890!@##$%^&*()_+"
-len_password = int(input("enter number of characters in password :"))
-password = "".join(random.sample(s, len_password))
-print(password)
+# with open("game.p", "br") as readfile:
+#   info = pickle.load(readfile)
+# print(info)
+# s = "QWERTYUIOPLKJHGFDSAZXCVBNM,.;'[]=-qwertyuioplkjhgfdsazxcvbnm,.;']*-1234567890!@##$%^&*()_+"
+# len_password = int(input("enter number of characters in password :"))
+# password = "".join(random.sample(s, len_password))
+# print(password)
 
-answer = input("would you like to keep this password y:")
-if ("y" in answer):
-    account_name = input("enter account name:")
-    info[account_name] = password
-    print(info)
-    with open("game.p", "bw") as filewrite:
-        pickle.dump(info, filewrite, protocol=2)
+# answer = input("would you like to keep this password y:")
+# if ("y" in answer):
+#     account_name = input("enter account name:")
+#     info[account_name] = password
+#     print(info)
+#     with open("game.p", "bw") as filewrite:
+#         pickle.dump(info, filewrite, protocol=2)
 
-else:
-    print("ok")
+# else:
+#     print("ok")
